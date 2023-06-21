5faf0a853897a41c57e7b01c15f3dd0ceebd01cc
Nghiem1005 <nghiem100514789123@gmail.com>
2023-06-21 09:34:17 +0700

    Update filed bill sql

--- src/main/java/com/example/english/repository/BillRepository.java
+++ src/main/java/com/example/english/repository/BillRepository.java
@@ -17,17 +17,17 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
   List<Bill> findBillsByCourse(Course course);
   @Query(value = "select course_id as courseId, count(course_id) as amount from tbl_bill group by course_id order by amount", nativeQuery = true)
   List<ICourseSeller> bestSeller();
-  @Query(value = "select sum(total_price) as price from tbl_bill", nativeQuery = true)
+  @Query(value = "select sum(price) as price from tbl_bill", nativeQuery = true)
   double totalPrice();
 
-  @Query(value = "select weekday(b.create_date) as weekDay, SUM(b.total_price) as totalValue  from tbl_bill as b \n"
+  @Query(value = "select weekday(b.create_date) as weekDay, SUM(b.price) as totalValue  from tbl_bill as b \n"
       + "inner join tbl_course as c on b.course_id = c.id\n"
       + "where c.course_type = :typeCourse \n"
       + "and b.create_date < now() and b.create_date > date_sub(now(), interval :amountDay Day)\n"
       + "group by weekday(create_date)", nativeQuery = true)
   List<IStatisticDay> findRevenueByDayAndTypeCourse(@Param("amountDay") int amountDay, @Param("typeCourse") String typeCourse);
 
-  @Query(value = "select weekday(create_date) as weekDay, SUM(total_price) as totalValue  from tbl_bill \n"
+  @Query(value = "select weekday(create_date) as weekDay, SUM(price) as totalValue  from tbl_bill \n"
       + "      where create_date < now() and create_date > date_sub(now(), interval :amountDay Day) \n"
       + "      group by weekday(create_date)", nativeQuery = true)
   List<IStatisticDay> findRevenueByDay(@Param("amountDay") int amountDay);
