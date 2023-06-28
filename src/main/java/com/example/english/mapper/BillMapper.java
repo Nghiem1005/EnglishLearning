b8753e476ac76f00994c03d78821d61c42035f09
Nghiem1005 <nghiem100514789123@gmail.com>
2023-06-28 22:25:29 +0700

    Update bill response

--- src/main/java/com/example/english/mapper/BillMapper.java
+++ src/main/java/com/example/english/mapper/BillMapper.java
@@ -3,11 +3,15 @@ package com.example.english.mapper;
 import com.example.english.dto.response.BillResponseDTO;
 import com.example.english.entities.Bill;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BillMapper {
   BillMapper INSTANCE = Mappers.getMapper(BillMapper.class);
 
+  @Mapping(target = "courseId", source = "bill.course.id")
+  @Mapping(target = "studentId", source = "bill.user.id")
+  @Mapping(target = "studentName", source = "bill.user.id")
   BillResponseDTO billToBillResponseDTO(Bill bill);
 }
