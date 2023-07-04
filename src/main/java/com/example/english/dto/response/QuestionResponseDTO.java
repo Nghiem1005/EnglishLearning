43ba956441bf188f92ca6365954282aa01acae7f
Nghiem1005 <nghiem100514789123@gmail.com>
2023-07-04 12:43:35 +0700

    Update response

--- src/main/java/com/example/english/dto/response/QuestionResponseDTO.java
+++ src/main/java/com/example/english/dto/response/QuestionResponseDTO.java
@@ -12,6 +12,6 @@ public class QuestionResponseDTO {
   private Long id;
   private String content;
   private int serial;
-  private String explain;
+  private String explainContent;
   private List<AnswerResponseDTO> answerResponseDTOS;
 }
