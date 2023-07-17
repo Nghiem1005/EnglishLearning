f5ea9dddde44838037329c041aaec28e730841b9
Nghiem1005 <nghiem100514789123@gmail.com>
2023-07-17 12:18:02 +0700

    Update API

--- src/main/java/com/example/english/controller/ExamController.java
+++ src/main/java/com/example/english/controller/ExamController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +34,11 @@ public class ExamController {
   @PostMapping(value = "/lesson")
   public ResponseEntity<?> createExamLesson(@RequestParam(name = "lessonId") Long lessonId ,@RequestBody ExamRequestDTO examRequestDTO) {
     return examService.createExamLesson(lessonId, examRequestDTO);
+  }
+
+  @PutMapping(value = "/lesson")
+  public ResponseEntity<?> updateExamLesson(@RequestParam(name = "examId") Long examId ,@RequestBody ExamRequestDTO examRequestDTO) {
+    return examService.updateExam(examId, examRequestDTO);
   }
 
   @GetMapping
